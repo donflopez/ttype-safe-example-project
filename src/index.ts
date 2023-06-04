582ba275966bffcae0f87034bea734a24bff5a7e
Francisco <donflopez@gmail.com>
2023-06-04 10:27:30 -0700

    feat(Examples): Include new throw error and custom tag example.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-import { $schema, validate } from "ttype-safe/validate"
+import { $schema, createCustomValidate, validate } from "ttype-safe/validate"
+
+const throwCatcher = (fn: Function) => {
+    try {
+        fn();
+    } catch (e) {
+        console.log(e);
+    }
+}
 
 type SimpleType = {
     /**
@@ -7,6 +15,7 @@ type SimpleType = {
      * @min 1
      * @max 8
      * @alphanumeric
+     * @startsWithA
      */
     input: string,
 }
@@ -17,3 +26,30 @@ console.log(SimpleTypeValidator({input: 'hello'})); // true
 console.log(SimpleTypeValidator({input: 'hello-'})); // false
 console.log(SimpleTypeValidator({input: ''})); // false
 console.log(SimpleTypeValidator({input: 'abcdefghi'})); // false
+
+
+// ENABLE THROWING ERRORS
+const validatorThatThrows = createCustomValidate({}, true);
+
+const SimpleTypeValidatorThrows = validatorThatThrows<SimpleType>($schema<SimpleType>());
+
+throwCatcher(() => SimpleTypeValidatorThrows({input: 'hello-'})); // Error: ValidationError: Tag validation [alphanumeric] and comment [null] didn't succeed for value [hello-]
+throwCatcher(() => SimpleTypeValidatorThrows({input: ''})); // Error: ValidationError: Tag validation [min] and comment [1] didn't succeed for value []
+throwCatcher(() => SimpleTypeValidatorThrows({input: 'abcdefghi'})); // ValidationError: Tag validation [max] and comment [8] didn't succeed for value [abcdefghi]
+
+// ADD CUSTOM TAGS
+const validatorThatThrows2 = createCustomValidate({
+    string: {
+        startsWithA: (value, _tagInput) => {
+            return !!value && (value[0] === 'a' || value[0] === 'A');
+        }
+    }
+}, true);
+
+const SimpleTypeValidatorThrows2 = validatorThatThrows2<SimpleType>($schema<SimpleType>());
+
+throwCatcher(() => SimpleTypeValidatorThrows2({input: 'Ahello'})); // Correct
+throwCatcher(() => SimpleTypeValidatorThrows2({input: 'hello'})); // Error: ValidationError: Tag validation [startsWithA] and comment [null] didn't succeed for value [hello]
+throwCatcher(() => SimpleTypeValidatorThrows2({input: 'hello-'})); // Error: ValidationError: Tag validation [alphanumeric] and comment [null] didn't succeed for value [hello-]
+throwCatcher(() => SimpleTypeValidatorThrows2({input: ''})); // Error: ValidationError: Tag validation [min] and comment [1] didn't succeed for value []
+throwCatcher(() => SimpleTypeValidatorThrows2({input: 'abcdefghi'})); // ValidationError: Tag validation [max] and comment [8] didn't succeed for value [abcdefghi]
